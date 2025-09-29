f17e4325dc57967f8379ddd40a18cca6f7515278
hamalfi-jp <hamalfi@gmail.com>
2025-09-29 15:16:57 +0900

    Deploy 2025-09-29T15:16:57+09:00

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Base path for GitHub Pages: https://hamalfi-jp.github.io/cards_view/
-  base: '/cards_view/',
-})
